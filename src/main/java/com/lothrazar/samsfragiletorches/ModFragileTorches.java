e02bbd475c81134b6d4f551718a90ff1ca44ad8d
sam bassett <samson.bassett@gmail.com>
2015-06-07 22:50:41 -0700

    empty mod shells: beetroot and fragile torches

---
+++ src/main/java/com/lothrazar/samsfragiletorches/ModFragileTorches.java
@@ -0,0 +1,20 @@
+package com.lothrazar.samsfragiletorches;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ModFragileTorches.MODID, version = ModFragileTorches.VERSION)
+public class ModFragileTorches
+{
+    public static final String MODID = "samsfragiletorches";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
