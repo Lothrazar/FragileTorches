3a69c43f0abc35a67ec6292e35d72ce60b433754
sam bassett <samson.bassett@gmail.com>
2015-06-10 10:11:38 -0700

    clean and fix mc info files. remove dead or nonfitting features

--- src/main/java/com/lothrazar/samsfragiletorches/ModFragileTorches.java
+++ src/main/java/com/lothrazar/samsfragiletorches/ModFragileTorches.java
@@ -13,11 +13,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = ModFragileTorches.MODID, version = ModFragileTorches.VERSION)
+@Mod(modid = ModFragileTorches.MODID, useMetadata=true)
 public class ModFragileTorches
 {
     public static final String MODID = "samsfragiletorches";
-    public static final String VERSION = "1.0";
+
 	@Instance(value = ModFragileTorches.MODID)
 	public static ModFragileTorches instance;
 	
