14d8f4c26b7e424b682fb7f915651f54e6f8c2a6
lothrazar <samson.bassett@gmail.com>
2020-02-05 23:17:13 -0800

    1.15.2 breakable refactor

--- src/main/java/com/lothrazar/fragiletorches/FragTorchEvent.java
+++ src/main/java/com/lothrazar/fragiletorches/FragTorchEvent.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.BlockTags.Wrapper;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -22,28 +23,33 @@ public class FragTorchEvent {
     if (ent == null) {
       return;
     }
+    if (ConfigManager.entityIsGentle(ent.getType())) {
+      return;
+    }
+    if (ent instanceof PlayerEntity) {
+      //i am a player, i can avoid this
+      PlayerEntity p = (PlayerEntity) ent;
+      if (p.isCrouching()) {
+        return;// ok // torches are safe from breaking as secret edge case for happiness
+      }
+    }
     World world = ent.world;
     if (ent.world.rand.nextDouble() > 0.01) {
       return;
     }
-    //    private static final BlockTags.Wrapper SLEAVES = new BlockTags.Wrapper(ScytheType.LEAVES.type());
-    BlockState bs = world.getBlockState(ent.getPosition());
-    //    if (bs.getBlock() == Blocks.TORCH) {
-    if (bs.isIn(TAGSTATE) && !ConfigManager.entityIsGentle(ent.getType())) {
-      //player? 
-      if (ent instanceof PlayerEntity) {
-        //i am a player, i can avoid this
-        PlayerEntity p = (PlayerEntity) ent;
-        if (p.isCrouching()) {
-          return;// ok // torches are safe from breaking as secret edge case for happiness
-        }
-      }
-      //ok break the torch
-      this.tryBreak(ent);
+    BlockPos pos = ent.getPosition();
+    BlockState bs = world.getBlockState(pos);
+    boolean breakable = bs.isIn(TAGSTATE);
+    if (!breakable && ent.getEyeHeight() >= 1) {
+      //also check above at eye level
+      pos = pos.up();//so go up one 
+      bs = world.getBlockState(pos);
+      breakable = bs.isIn(TAGSTATE);
     }
-  }
-
-  private void tryBreak(Entity living) {
-    living.world.destroyBlock(living.getPosition(), true);
+    if (breakable) {
+      //player? 
+      //ok break the torch
+      ent.world.destroyBlock(pos, true);
+    }
   }
 }
